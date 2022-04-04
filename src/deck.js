3bc654a6f368acab90649006abb02147c3b25135
Sylvain Nieuwlandt <nieuwlandt.s@an0rak.dev>
2022-04-05 00:15:11 +0200

    Completly generate the html file

---
+++ src/deck.js
@@ -0,0 +1,81 @@
+const { JSDOM } = require("jsdom");
+
+module.exports = class Deck {
+    constructor(title, revealJsRoot) {
+        this.plugins = {};
+        this.revealJsRoot = revealJsRoot;
+        this.jsdom = new JSDOM("<!doctype html><html></html>");
+        this.document = this.jsdom.window.document;
+
+        const titleTag = this.document.createElement("title");
+        titleTag.innerHTML = title;
+        this.document.head.appendChild(titleTag);
+
+        const encodingTag = this.document.createElement("meta");
+        encodingTag.setAttribute("charset", "utf-8");
+        this.document.head.appendChild(encodingTag);
+
+        const viewportTag = this.document.createElement("meta");
+        viewportTag.setAttribute("name", "viewport");
+        viewportTag.setAttribute("content", "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no");
+        this.document.head.appendChild(viewportTag);
+
+        
+        this.addStylesheet(`${this.revealJsRoot}/dist/reset.css`);
+        this.addStylesheet(`${this.revealJsRoot}/dist/reveal.css`);
+
+        
+        const revealDiv = this.document.createElement("div");
+        revealDiv.setAttribute("class", "reveal");
+        const deckDiv = this.document.createElement("div");
+        deckDiv.setAttribute("id", "deck");
+        deckDiv.setAttribute("class", "slides");
+        revealDiv.appendChild(deckDiv);
+        this.document.body.appendChild(revealDiv);
+
+        this.addScriptRef(`${this.revealJsRoot}/dist/reveal.js`);
+    }
+
+    addStylesheet(link) {
+        const linkTag = this.document.createElement("link");
+        linkTag.setAttribute("rel", "stylesheet");
+        linkTag.setAttribute("href", link);
+        this.document.head.appendChild(linkTag);
+    }
+
+    useTheme(themeName) {
+        this.addStylesheet(`${this.revealJsRoot}/dist/theme/${themeName}.css`);
+    }
+
+    addSection(content) {
+        const slidesRoot = this.document.getElementById("deck");
+        const section = this.document.createElement("section");
+        section.setAttribute("data-markdown", "");
+        section.setAttribute("data-separator-vertical", "\\n---");
+        section.innerHTML = content;
+        slidesRoot.appendChild(section);
+    }
+
+    addPlugin(pluginName, pluginPath) {
+        this.plugins[pluginName] = pluginPath;
+    }
+
+    export() {
+        let pluginInit = "";
+        for (const [pluginName, pluginPath] of Object.entries(this.plugins)) {
+            this.addScriptRef(pluginPath);
+            pluginInit = `${pluginInit}, ${pluginName}`;
+        }
+        pluginInit = pluginInit.substring(1); // Remove trailing ","
+
+        const script = this.document.createElement("script");
+        script.innerHTML = `
+            Reveal.initialize({
+                hash: true,
+                plugins: [ ${pluginInit} ]
+            });
+        `;
+        this.document.body.appendChild(script);
+        return this.jsdom.serialize();
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { JSDOM } = require("jsdom");
+const Deck = require("./deck");
 
 const fs = require("fs").promises;
 const path = require("path");
@@ -10,58 +10,20 @@ const REVEALJS_ROOT = "./node_modules/reveal.js";
 
 const TITLE = "Initiation to 3D";
 
-function createStylesheetLink(document, target) {
-    const link = document.createElement("link");
-    link.setAttribute("rel", "stylesheet");
-    link.setAttribute("href", target);
-    document.head.appendChild(link);
-}
-
-function createScriptRef(document, target) {
-    const script = document.createElement("script");
-    script.setAttribute("src", target);
-    document.body.appendChild(script);
-}
-
 async function main() {
-    const templateRaw = await fs.readFile(path.join(RESOURCES, "template.html"), { encoding: "utf8" });
-    const templateDom = new JSDOM(templateRaw);
-    const document = templateDom.window.document;
+    const slideDeck = new Deck(TITLE, REVEALJS_ROOT);
+    slideDeck.useTheme("black");
 
-    // Setup Head
-    const title = document.createElement("title");
-    title.innerHTML = TITLE;
-    document.head.appendChild(title);
-
-    createStylesheetLink(document, `${REVEALJS_ROOT}/dist/reset.css`);
-    createStylesheetLink(document, `${REVEALJS_ROOT}/dist/reveal.css`);
-    createStylesheetLink(document, `${REVEALJS_ROOT}/dist/theme/black.css`);
-    createStylesheetLink(document, `${REVEALJS_ROOT}/plugin/highlight/monokai.css`);
-
-    const slidesRoot = document.getElementById("deck");
+    slideDeck.addStylesheet(`${REVEALJS_ROOT}/plugin/highlight/monokai.css`);
+    
     const slideContent = await fs.readFile(path.join(SLIDES, "test.md"));
-    const section = document.createElement("section");
-    section.setAttribute("data-markdown", "");
-    section.setAttribute("data-separator-vertical", "\\n---");
-    section.innerHTML = slideContent;
-    slidesRoot.appendChild(section);
+    slideDeck.addSection(slideContent);
 
-    // Add JS scripts
-    createScriptRef(document, `${REVEALJS_ROOT}/dist/reveal.js`);
-    createScriptRef(document, `${REVEALJS_ROOT}/plugin/notes/notes.js`);
-    createScriptRef(document, `${REVEALJS_ROOT}/plugin/markdown/markdown.js`);
-    createScriptRef(document, `${REVEALJS_ROOT}/plugin/highlight/highlight.js`);
+    slideDeck.addPlugin("RevealNotes", `${REVEALJS_ROOT}/plugin/notes/notes.js`);
+    slideDeck.addPlugin("RevealMarkdown", `${REVEALJS_ROOT}/plugin/markdown/markdown.js`);
+    slideDeck.addPlugin("RevealHighlight", `${REVEALJS_ROOT}/plugin/highlight/highlight.js`);
 
-    const revealInitScript = document.createElement("script");
-    revealInitScript.innerHTML = `
-        Reveal.initialize({
-            hash: true,
-            plugins: [ RevealMarkdown, RevealHighlight, RevealNotes ]
-        });
-    `;
-    document.body.appendChild(revealInitScript);
-
-    fs.writeFile(path.join(PROJECT_ROOT, "index.html"), templateDom.serialize(), { encoding: "utf8" });
+    fs.writeFile(path.join(PROJECT_ROOT, "index.html"), slideDeck.export(), { encoding: "utf8" });
 }
 
 main();
